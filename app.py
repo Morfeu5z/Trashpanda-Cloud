6a15ea5239d22ba111863e66250a59301022c018
MikusRy <mikolaj.rychel@gmail.com>
2018-08-14 15:54:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ app.register_blueprint(searchEngine)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'dendarrus@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Klaudynka123'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail()
